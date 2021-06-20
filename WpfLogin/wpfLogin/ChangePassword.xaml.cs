d4bb4870c5093947cd2eda46764bcdeb86a331db
Prasath <prasathravindu2@gmail.com>
2021-06-20 12:04:29 +0530

    Add files via upload

---
+++ WpfLogin/wpfLogin/ChangePassword.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfLogin
+{
+    /// <summary>
+    /// Interaction logic for ChangePassword.xaml
+    /// </summary>
+    public partial class ChangePassword : Page
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new LoginPage());
+        }
+    }
+}
---
+++ WpfLogin/wpfLogin/LoginPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfLogin
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSign_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new signup());
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ChangePassword());
+        }
+    }
+}
